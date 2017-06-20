b2b4dcd98543737890b38488bd4e551d7996c750
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-20 10:56:30 +0900

    rubocopfy

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,20 +1,21 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-localtime_zone = 'Japan'
+localtime_zone = "Japan"
 
 case os[:family]
-when 'freebsd'
-  localtime_zone = 'Asia/Tokyo'
+when "freebsd"
+  localtime_zone = "Asia/Tokyo"
 end
 
-describe file('/etc/localtime') do
+describe file("/etc/localtime") do
   it { should exist }
   it { should be_symlink }
-  it { should be_linked_to "/usr/share/zoneinfo/#{ localtime_zone }" }
+  it { should be_linked_to "/usr/share/zoneinfo/#{localtime_zone}" }
 end
 
-describe command('date +%z') do
-  its(:stdout) { should match /^\+0900$/ }
-  its(:stderr) { should match /^$/ }
+describe command("date +%z") do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match(/^\+0900$/) }
+  its(:stderr) { should eq "" }
 end
